739bf4ab7c65348e1bb66129e9e3ccde9c65cec9
Mixi <45658580+Mixi@users.noreply.github.com>
2019-10-21 23:47:21 +0100

    Updated wyr

--- src/commands/Fun & Games/wouldyourather.js
+++ src/commands/Fun & Games/wouldyourather.js
@@ -19,26 +19,27 @@ module.exports = class extends Command {
   async run(message) {
     const choice = Math.floor((Math.random() * 483514) + 0);
     const url = 'http://www.either.io/';
+    const thumbnail = 'https://pbs.twimg.com/profile_images/1729848996/yr-twitter_400x400.png';
     const title = 'Either.io';
     const author = 'Would you rather?';
     const link = url + choice;
+    const display = new MessageEmbed()
+      .setTitle(title)
+      .setAuthor(author)
+      .setColor(color)
+      .setURL(link)
+      .setThumbnail(thumbnail)
+      .setFooter('© Either.io');
 
-    const display = request(link)
+    await request(link)
       .then(html => {
         const $ = cheerio.load(html);
-        const thumbnail = 'https://pbs.twimg.com/profile_images/1729848996/yr-twitter_400x400.png';
         const optionA = $('.panel').find('.result-1').first().children('.option-text').text();
         const optionB = $('.panel').find('.result-2').first().children('.option-text').text();
         const info = $('.more-info').text();
 
-        return new MessageEmbed()
-          .setTitle(title)
-          .setAuthor(author)
-          .setColor(color)
-          .setURL(link)
-          .setThumbnail(thumbnail)
-          .setDescription(`**Either...**\nA) ${optionA} 🔵\n\n__**OR**__\n\nB) ${optionB} 🔴\n${info}`)
-          .setFooter('© Either.io');
+        display.setDescription(`**Either...**\nA) ${optionA} 🔵\n\n__**OR**__\n\nB) ${optionB} 🔴\n${info}`);
+
       })
       .catch(error => {
         if (error.statusCode === 403) throw message.send('Either.io is down, try again later, darling.');
